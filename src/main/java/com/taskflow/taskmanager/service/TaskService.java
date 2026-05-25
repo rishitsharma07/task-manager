111204a13dc296305387c5f243569c90035c56d2
rishitsharma07 <rishitsharma1675@gmail.com>
2026-05-25 17:28:02 +0530

    feat: add jwt dependencies and complete service layer

--- src/main/java/com/taskflow/taskmanager/service/TaskService.java
+++ src/main/java/com/taskflow/taskmanager/service/TaskService.java
@@ -4,7 +4,6 @@ import com.taskflow.taskmanager.dto.TaskRequest;
 import com.taskflow.taskmanager.entity.Task;
 import com.taskflow.taskmanager.entity.User;
 import com.taskflow.taskmanager.repository.TaskRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +11,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 @RequiredArgsConstructor
 public class TaskService {
 
     private final TaskRepository taskRepository;
 
-    public void createTask(TaskRequest taskRequest, User user){
+    public Task createTask(TaskRequest taskRequest, User user){
 
         Task task = Task.builder()
+                .user(user)
                 .title(taskRequest.getTitle())
                 .description(taskRequest.getDescription())
                 .priority(taskRequest.getPriority())
@@ -30,7 +29,7 @@ public class TaskService {
                 .updatedAt(LocalDateTime.now())
                 .build();
 
-        taskRepository.save(task);
+        return taskRepository.save(task);
 
     }
 
@@ -38,5 +37,37 @@ public class TaskService {
         return taskRepository.findByUser(user);
     }
 
+    public Task updateTask(Long id, TaskRequest taskRequest, User user){
+
+        Task task1 = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+        if(!task1.getUser().getId().equals(user.getId())){
+            throw new RuntimeException("Unauthorized");
+        }
+
+        task1.setTitle(taskRequest.getTitle());
+        task1.setDescription(taskRequest.getDescription());
+        task1.setPriority(taskRequest.getPriority());
+        task1.setStatus(taskRequest.getStatus());
+        task1.setDueDate(taskRequest.getDueDate());
+        task1.setUpdatedAt(LocalDateTime.now());
+
+        return taskRepository.save(task1);
+
+    }
+
+    public void deleteTask(Long id, User user){
+
+        Task task2 = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+        if (!task2.getUser().getId().equals(user.getId())){
+            throw new RuntimeException("Unauthorized");
+        }
+
+        taskRepository.deleteById(id);
+
+    }
 
 }
