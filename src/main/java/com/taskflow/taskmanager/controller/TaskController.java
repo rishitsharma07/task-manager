ae2fa3736d5da5a493ae289052c1f5fb43d2894e
rishitsharma07 <rishitsharma1675@gmail.com>
2026-05-29 14:45:13 +0530

    feat: add task controller

--- src/main/java/com/taskflow/taskmanager/controller/TaskController.java
+++ src/main/java/com/taskflow/taskmanager/controller/TaskController.java
@@ -1,13 +1,51 @@
 package com.taskflow.taskmanager.controller;
 
+import com.taskflow.taskmanager.dto.TaskRequest;
+import com.taskflow.taskmanager.entity.User;
+import com.taskflow.taskmanager.service.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/tasks")
 @RequiredArgsConstructor
 public class TaskController {
 
+    private final TaskService taskService;
 
+    @GetMapping
+    public ResponseEntity<?> getAllTasks(){
+        User user = (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        return ResponseEntity.ok(taskService.getTasksByUser(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewTasks(@RequestBody TaskRequest taskRequest){
+        User user = (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        return ResponseEntity.ok(taskService.createTask(taskRequest, user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTask(@PathVariable Long id,@RequestBody TaskRequest taskRequest){
+        User user = (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        return ResponseEntity.ok(taskService.updateTask(id, taskRequest, user));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable Long id){
+        User user = (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        taskService.deleteTask(id,user);
+        return ResponseEntity.ok("Task deleted Sucessfully");
+    }
 }
